7cbfa1a9a2261c7bc38c13b84c9cd8176d9c74b0
Jason Downing <jason@downing.io>
2019-03-31 23:33:36 -0400

    trying to get the imputation.py script to run

--- python/imputation.py
+++ python/imputation.py
@@ -7,14 +7,11 @@ imputed_data = raw_data.copy()
 
 # -------- specs to be imputed with mean, and the rest with mode
 specs_to_mean = ['MSRP', 'Base Curb Weight (lbs)', 'Second Shoulder Room (in)', 'Second Head Room (in)', 'Front Shoulder Room (in)',
-                'Second Hip Room (in)', 'Front Head Room (in)', 'Second Leg Room (in)', 'Front Hip Room (in)',
-                'Front Leg Room (in)', 'Width, Max w/o mirrors (in)', 'Track Width, Rear (in)', 'Height, Overall (in)',
-                'Wheelbase (in)', 'Track Width, Front (in)', 'Fuel Tank Capacity, Approx (gal)', 'Fourth Gear Ratio (:1)',
-                'Second Gear Ratio (:1)', 'Reverse Ratio (:1)', 'Fifth Gear Ratio (:1)', 'Third Gear Ratio (:1)',
-                'Final Drive Axle Ratio (:1)', 'First Gear Ratio (:1)', 'Sixth Gear Ratio (:1)',
-                'Front Brake Rotor Diam x Thickness (in)', 'Rear Brake Rotor Diam x Thickness (in)', 'Displacement (cc)',
-                'Displacement (L)', 'Net Torque RPM', 'Net Torque', 'Net Horsepower', 'Net Horsepower RPM',
-                'Passenger Volume', 'Turning Diameter - Curb to Curb']
+                'Front Head Room (in)', 'Second Leg Room (in)', 'Front Hip Room (in)', 'Front Leg Room (in)', 'Width, Max w/o mirrors (in)',
+                'Track Width, Rear (in)', 'Height, Overall (in)', 'Wheelbase (in)', 'Track Width, Front (in)', 'Fuel Tank Capacity, Approx (gal)',
+                'Fourth Gear Ratio (:1)', 'Second Gear Ratio (:1)', 'Reverse Ratio (:1)', 'Fifth Gear Ratio (:1)', 'Third Gear Ratio (:1)',
+                'Final Drive Axle Ratio (:1)', 'First Gear Ratio (:1)', 'Displacement (cc)', 'Displacement (L)', 'Net Torque RPM',
+                'Net Torque', 'Net Horsepower', 'Net Horsepower RPM', 'Passenger Volume', 'Turning Diameter - Curb to Curb']
 
 for i in specs_to_mean:
     try:
@@ -51,7 +48,7 @@ def imputer_mean(x, y):
                 imputed_data.loc[x, y] = np.round(temp_list_bodystyle.mean(), 2)
 
 def imputer_mode(x, y):
-    temp_list_model = pd.Series(raw_data.loc[raw_data["Model"] == raw_data.loc[x, "Model"]].loc[:, y].dropna())
+    temp_list_model = pd.Series(raw_data.loc[raw_data["Body Style"] == raw_data.loc[x, "Body Style"]].loc[:, y].dropna())
     if len(temp_list_model) > 0:
         imputed_data.loc[x, y] = stats.mode(temp_list_model)[0][0]
     else:
